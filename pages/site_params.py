2aa276f940cea9314ee6ba1df76ee74b7ef3bb04
xevb3k <jorom2004@mail.ru>
2022-08-30 23:26:42 +0300

    v.1.2

--- pages/site_params.py
+++ pages/site_params.py
@@ -18,23 +18,3 @@ class MVideoLinks:
     link_home_page_for_business = '/mvideo-biznes'
     link_home_page_main_menu_all_stocks = '/vse-akcii'
  
-
-    main_menu_urls_1 = [
-        '/best/',
-        '/books/',
-        '/school/',
-        '/office/',
-        '/games/'
-
-    ]
-
-    main_menu_urls_2 = [
-        'https://www.labirint.ru/help/', 'https://www.labirint.ru/top/certificates/',
-        'https://www.labirint.ru/rating/?id_genre=-1&nrd=1',
-        'https://www.labirint.ru/novelty/',
-        'https://www.labirint.ru/search/?discount=1&available=1&order=actd&way=back&paperbooks=1&ebooks=1&otherbooks=1',
-        'https://www.labirint.ru/contact/'
-    ]
-
-    btn_help = '/help/'
-
